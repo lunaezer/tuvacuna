495a84bb2963c1ed7db6f7dfe56880b58fd24dbd
lunaezer <lunaezer22@gmail.com>
2026-08-12 15:12:48 -0300

    card

---
+++ tu vacuna/src/components/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Cards.css'
+
+function Cards ({text, variant, onClick}) {
+    return(
+        <div
+        className={"variant"}
+        onClick={onClick}
+        >
+        </div> 
+    )
+}
