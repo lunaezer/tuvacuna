489b217c7dd79cfb85a0f91b4ae873bd935f4062
lunaezer <lunaezer22@gmail.com>
2026-08-20 08:12:28 -0300

    feat: add HomeLogo asset and wrap SidebarHeader in a navigation link to home

--- tu vacuna/src/components/Sidebar/Sidebar.jsx
+++ tu vacuna/src/components/Sidebar/Sidebar.jsx
@@ -3,16 +3,20 @@ import SidebarHeader from './SidebarHeader'
 import SidebarItem from './SidebarItem'
 import SidebarFooter from './SidebarFooter'
 import './Sidebar.css'
+import HomeLogo from '../../assets/HomeLogo.png'
+
 
 function Sidebar({ activeItem = 'Panel', className = '' }) {
   return (
     <aside className={`sidebar ${className}`.trim()}>
-      <SidebarHeader >
+      <SidebarHeader to="/">
         <h1 className='titulo-sidebar'>TuVacuna</h1>
         </SidebarHeader>
 
       <nav className="sidebar-nav">
-        <SidebarItem text="Panel" to="/panel" active={activeItem === 'Panel'} />
+        <SidebarItem text="Panel" to="/panel" active={activeItem === 'Panel'} >
+        <img src={HomeLogo}alt='HomeLogo' className='HomeLogo'></img>
+        </SidebarItem>
         <SidebarItem text="Mi carnet" to="/mi-carnet" active={activeItem === 'Mi carnet'} />
         <SidebarItem text="Calendario" to="/calendario" active={activeItem === 'Calendario'} />
         <SidebarItem text="Centros" to="/centros" active={activeItem === 'Centros'} />
--- tu vacuna/src/components/Sidebar/SidebarHeader.jsx
+++ tu vacuna/src/components/Sidebar/SidebarHeader.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './SidebarHeader.css'
+import { Link } from 'react-router-dom'
 
-function SidebarHeader({ children, className = ''}) {
+function SidebarHeader({ children, className = '', to }) {
   return (
     <div className={`sidebar-header ${className}`.trim()}>
-      {children}
-      
+      <Link to={to} className="sidebar-header-link">
+        {children}
+      </Link>
     </div>
   )
 }
